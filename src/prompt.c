3f32b839d4da95f87c2b087d587f67b89a6e928a
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2020-01-17 12:45:03 +0200

    cursor pos is defined by shell size

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 08:53:12 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/01/17 12:25:40 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/01/17 12:41:43 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,15 +53,7 @@ void	print_debug(t_shell *sh)
 void	print_input(t_shell *sh)
 {
 	ft_printf("\033[s");
-
-
-	// CURSOR(24, 11);
-	// ft_printf("%s", sh->input);
-	// ft_printf("\033[u");
-
-
-	// ft_printf("\033[24;11f%-*s\033[u", sh->len, sh->input);
-	ft_printf("\033[%d;%df%-*s\033[u", 10, 10, sh->len, sh->input);
+	ft_printf("\033[%d;%df%-*s\033[u", sh->rows, sh->prompt_len + 1, sh->len, sh->input);
 }
 
 void	get_shell_size(t_shell *sh)
--- src/setup.c
+++ src/setup.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 18:35:03 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/01/17 11:58:20 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/01/17 12:42:20 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ t_shell *create_shell(void)
 
 	sh = (t_shell *)ft_memalloc(sizeof(t_shell));
 	sh->i = 0;
-	sh->y = 13;
+	sh->y = 0;
 	sh->len = 0;
 	sh->key = 0;
 	// TODO: If doesn't exist
