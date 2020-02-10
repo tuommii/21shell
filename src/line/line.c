a3397bc50828c799627c25efa8c464d1e8bedd2e
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2020-02-10 10:28:14 +0200

    somekind progress

--- src/line/line.c
+++ src/line/line.c
@@ -6,25 +6,38 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 07:10:34 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/02/06 07:54:22 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/02/10 10:27:55 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void prompt() {
+static void prompt(char *input) {
 	ft_printf("%-*s", 2, "$>");
+	ft_putstr(tgetstr("sc", NULL));
 }
 
 void start_line_editor() {
-	prompt();
 	char input[INPUT_BUFFER];
 	ft_bzero(input, INPUT_BUFFER);
-	int x = 0;
+	prompt(input);
+
+	int prompt_len = 2;
+	int x = prompt_len;
 	int len = 0;
+	int i = 0;
+
 	while (1) {
 		listen_signals();
 		watch_kill();
+
+		int j = 0;
+
+		ft_putstr(tgetstr("rc", NULL));
+		while (j < len) {
+			ft_putchar(input[j]);
+			j++;
+		}
 		int key = keypress();
 		if (key == ESC)
 		{
@@ -32,26 +45,39 @@ void start_line_editor() {
 		}
 		else if (ft_isprint(key)) {
 			// ft_putchar(key);
+			if ((x - prompt_len) < prompt_len)
+			{
+				ft_insert(input, x - prompt_len + 1, (char)key);
+			}
+			else if (x - prompt_len == len)
+			{
+				input[i] = (char)key;
+				i++;
+			}
 			len++;
-			ft_putchar(key);
-			input[x] = (char)key;
+			// ft_putchar(key);
 			// ft_insert(input, x, (char)key);
 			x++;
 		}
 		else if (key == ENTER) {
-			ft_printf(input);
+			//ft_printf("\tINPUT: %s\n", input);
 			ft_putstr(tgetstr("do", NULL));
 			ft_bzero(input, INPUT_BUFFER);
-			x = 0;
-			prompt();
+			x = prompt_len;
+			len = 0;
+			i = 0;
+			prompt(input);
 		}
 		else if (key == LEFT) {
-			move_left();
+			if (x > prompt_len) {
+				move_left();
+				x--;
+			}
 		}
 		else if (key == RIGHT) {
 			move_right();
+			x++;
 		}
-		// ft_putstr(input);
 	}
 
 }
