14e4992d08c8654746bdbe6a60c9fedfbf0797ee
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2020-02-10 11:47:55 +0200

    removed unneccassry lines

--- src/line/line.c
+++ src/line/line.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 07:10:34 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/02/10 11:41:16 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/02/10 11:47:42 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,12 @@ void start_line_editor() {
 	while (1) {
 		listen_signals();
 		watch_kill();
-		// ft_printf("%-*s", len, input);
 		int key = keypress();
 		if (key == ESC)
 		{
 			return ;
 		}
 		else if (ft_isprint(key)) {
-			// ft_putchar(key);
-			// if ((x - prompt_len) < prompt_len)
-			// {
-			// 	ft_insert(input, x - prompt_len + 1, (char)key);
-			// }
-			// else if (x - prompt_len == len)
-			// {
-				// i++;
 			input[i] = (char)key;
 			i++;
 			len++;
@@ -53,31 +44,21 @@ void start_line_editor() {
 			ft_printf("%-*s", len, input);
 		}
 		else if (key == ENTER) {
-			//ft_printf("\tINPUT: %s\n", input);
 			ft_putstr(tgetstr("do", NULL));
-			ft_putstr(tgetstr("sc", NULL));
 			ft_bzero(input, INPUT_BUFFER);
 			x = prompt_len;
 			len = 0;
 			i = 0;
-			prompt(input);
+			prompt();
 		}
 		else if (key == LEFT) {
-			//if (x > prompt_len) {
-				// ft_putstr(tgetstr("sc", NULL));
 				move_left();
 				x--;
-			//}
 		}
 		else if (key == RIGHT) {
 			move_right();
 			x++;
 		}
-		// while (j < len) {
-		// 	ft_putchar(input[j]);
-		// 	j++;
-		// }
-		// ft_putstr(tgetstr("sc", NULL));
 	}
 
 }
