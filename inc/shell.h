8676dd4e02548a9827100f41eaa1f5c74a5863d5
Rouhija <sami_rouhe@hotmail.com>
2020-04-18 13:25:38 +0300

    norm checks in .h

--- inc/shell.h
+++ inc/shell.h
@@ -6,7 +6,7 @@
 /*   By: srouhe <srouhe@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 16:59:25 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/04/18 13:14:59 by srouhe           ###   ########.fr       */
+/*   Updated: 2020/04/18 13:25:18 by srouhe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,21 +46,21 @@
 /*
 ** Error messages
 */
-# define STR_MALLOC_ERR		"21sh: malloc error."
-# define STR_FORK_ERR		"21sh: failed to create child process."
-# define STR_EXECVE_ERR		"21sh: execve error."
-# define STR_SYNTAX_ERR		"21sh: syntax error near unexpected token"	
+# define STR_MALLOC_ERR	"21sh: malloc error."
+# define STR_FORK_ERR "21sh: failed to create child process."
+# define STR_EXECVE_ERR	"21sh: execve error."
+# define STR_SYNTAX_ERR	"21sh: syntax error near unexpected token"
 # define STR_PERMISSION_ERR	"21sh: permission denied:"
-# define STR_NOT_FOUND_ERR	"21sh: no such file or directory:"
-# define STR_NOT_DIR_ERR	"21sh: not a directory:"
-# define STR_IS_DIR			"21sh: is a directory:"
-# define STR_PIPE_ERR		"21sh: pipe error"	
-# define STR_DUP_ERR		"21sh: dup error."	
-# define STR_REDIR_ERR		"21sh: redirection error."
-# define STR_AMB_REDIR_ERR	"21sh: ambiguous redirection:"
-# define STR_EOF_ERR		"21sh: unexpected EOF while looking for matching"	
-# define STR_HERE_ERR		"21sh: warning: here-document delimited by end-of-file"	
-# define STR_BAD_FD_ERR		"21sh: Bad file descriptor:"	
+# define STR_NOT_FOUND_ERR "21sh: no such file or directory:"
+# define STR_NOT_DIR_ERR "21sh: not a directory:"
+# define STR_IS_DIR "21sh: is a directory:"
+# define STR_PIPE_ERR "21sh: pipe error"
+# define STR_DUP_ERR "21sh: dup error."
+# define STR_REDIR_ERR "21sh: redirection error."
+# define STR_AMB_REDIR_ERR "21sh: ambiguous redirection:"
+# define STR_EOF_ERR "21sh: unexpected EOF while looking for matching"
+# define STR_HERE_ERR "21sh: warning: here-document delimited by end-of-file"
+# define STR_BAD_FD_ERR	"21sh: Bad file descriptor:"
 
 /*
 ** Definitions of termcaps tuples:
@@ -81,7 +81,7 @@
 # define TI (tgetstr("ti", NULL))
 # define TE (tgetstr("te", NULL))
 
-int 					g_kill;
+int						g_kill;
 
 typedef struct			s_shell
 {
@@ -101,12 +101,12 @@ t_shell					g_sh;
 */
 
 void					print_error(int err, char *msg);
-void 					cleanup(t_line *line);
+void					cleanup(t_line *line);
 void					create_shell(char **environ, t_line *line);
 void					print_debug(void);
 void					parser_debug(t_ast *ast);
 void					lexer_debug(t_lexer *lexer);
-void 					array_debug(char **arr);
+void					array_debug(char **arr);
 void					ast_debug(t_ast *root, int ws);
 void					exit_error(int err, char *msg);
 
