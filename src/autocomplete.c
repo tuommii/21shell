7f949d1182f92874c93525bafc761faf03fe34ed
Miikka Tuominen <tuommii@gmail.com>
2020-07-03 15:27:49 +0300

    still working on CA-autocompletion

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 13:36:24 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/07/03 14:38:15 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/07/03 15:26:33 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,40 @@
 
 // TODO: Enum?
 # define CTX_BUILTIN "BUILTIN"
-# define CTX_BINARY "BINARY"
+# define CTX_BINARY "BINARY" // Might be same that builtin
+
 # define CTX_PATH "PATH"
 # define CTX_FILE "FILE"
+
 # define CTX_ENV "ENV"
 # define CTX_FLAG "FLAG"
 # define CTX_DISCARD "DISCARD"
 
 
-char *get_context(char buffer[INPUT_BUFFER], int cursor)
+
+// TODO: Reset context with  "|;" and what else?
+# define CTX_RESET "|;"
+
+static char *check_params(char buffer[INPUT_BUFFER], int cursor)
 {
 	if (!buffer || !cursor)
 		return (CTX_BUILTIN);
 
-	if (cursor > INPUT_BUFFER)
+	if (cursor > strlen(buffer))
 		return (CTX_DISCARD);
+
+	if (buffer[cursor] == ' ' && buffer[cursor - 1] == ' ')
+		return (CTX_DISCARD);
+
+	return (NULL);
+}
+
+char *get_context(char buffer[INPUT_BUFFER], int cursor)
+{
+	char *ctx;
+
+	if ((ctx = check_params(buffer, cursor)))
+		return (ctx);
 
 	// Discard extra spaces
 	while (buffer[cursor] == ' ')
@@ -41,6 +60,7 @@ char *get_context(char buffer[INPUT_BUFFER], int cursor)
 	if (cursor && buffer[cursor - 1] == ' ')
 	{
 		// Cursor is top of $ or -
+		// '/' is valid so not checking that
 		if (buffer[cursor] == '$' || buffer[cursor] == '-')
 			return (CTX_DISCARD);
 	}
@@ -51,6 +71,8 @@ char *get_context(char buffer[INPUT_BUFFER], int cursor)
 		return (CTX_BUILTIN);
 	else if (buffer[cursor + 1] == '-')
 		return (CTX_FLAG);
+	else if (buffer[cursor + 1] == '/')
+		return (CTX_PATH);
 	else if (buffer[cursor + 1] == '$')
 		return (CTX_ENV);
 	return (CTX_DISCARD);
@@ -58,24 +80,14 @@ char *get_context(char buffer[INPUT_BUFFER], int cursor)
 
 int main() {
 
-	char test[INPUT_BUFFER] = "ls  -a $HOME";
-	printf("%s\n", get_context(test, 0));
-	printf("%s\n", get_context(test, 1));
-	printf("%s\n", get_context(test, 2));
-	printf("%s\n", get_context(test, 3));
-	printf("==============================\n");
-	printf("%s\n", get_context(test, 4));
-	printf("==============================\n");
-	printf("%s\n", get_context(test, 5));
-	printf("%s\n", get_context(test, 6));
-	printf("==============================\n");
-	printf("%s\n", get_context(test, 7));
-	printf("==============================\n");
-	printf("%s\n", get_context(test, 8));
-	printf("%s\n", get_context(test, 9));
-	printf("%s\n", get_context(test, 10));
-	printf("%s\n", get_context(test, 11));
-	printf("%s\n", get_context(test, 12));
+	char test[INPUT_BUFFER] = "ls  -all $HOME   /home";
+	char test2[INPUT_BUFFER] = "echo  $HOME";
 
+	int i = 0;
+	while (i < strlen(test))
+	{
+		printf("cursor: %c, %s\n", test[i], get_context(test, i));
+		i++;
+	}
 	return (0);
 }
