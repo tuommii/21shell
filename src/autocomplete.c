7eaf023ff5eaea527ff775b06abaeed09c6d864d
Miikka Tuominen <tuommii@gmail.com>
2020-07-04 12:47:41 +0300

    somekind pipe recognition

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 13:36:24 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/07/03 15:26:33 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/07/04 12:46:41 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@
 
 
 // TODO: Reset context with  "|;" and what else?
-# define CTX_RESET "|;"
+# define CTX_DISCARD_STR "|;<>-$"
 
-static char *check_params(char buffer[INPUT_BUFFER], int cursor)
+static char *check_without_moving(char buffer[INPUT_BUFFER], int cursor)
 {
 	if (!buffer || !cursor)
 		return (CTX_BUILTIN);
@@ -43,6 +43,9 @@ static char *check_params(char buffer[INPUT_BUFFER], int cursor)
 	if (buffer[cursor] == ' ' && buffer[cursor - 1] == ' ')
 		return (CTX_DISCARD);
 
+	if (strchr(CTX_DISCARD_STR, buffer[cursor]))
+		return (CTX_DISCARD);
+
 	return (NULL);
 }
 
@@ -50,24 +53,21 @@ char *get_context(char buffer[INPUT_BUFFER], int cursor)
 {
 	char *ctx;
 
-	if ((ctx = check_params(buffer, cursor)))
+	if ((ctx = check_without_moving(buffer, cursor)))
 		return (ctx);
 
 	// Discard extra spaces
 	while (buffer[cursor] == ' ')
 		cursor--;
 
-	if (cursor && buffer[cursor - 1] == ' ')
-	{
-		// Cursor is top of $ or -
-		// '/' is valid so not checking that
-		if (buffer[cursor] == '$' || buffer[cursor] == '-')
-			return (CTX_DISCARD);
-	}
 
 	while (cursor && buffer[cursor] != ' ')
 		cursor--;
 	if (!cursor)
+		return (CTX_BUILTIN);
+	else if (buffer[cursor -1 ] == '|')
+		return (CTX_BUILTIN);
+	else if (buffer[cursor + 1] == '|')
 		return (CTX_BUILTIN);
 	else if (buffer[cursor + 1] == '-')
 		return (CTX_FLAG);
@@ -75,12 +75,15 @@ char *get_context(char buffer[INPUT_BUFFER], int cursor)
 		return (CTX_PATH);
 	else if (buffer[cursor + 1] == '$')
 		return (CTX_ENV);
+	else
+		return (CTX_PATH);
+
 	return (CTX_DISCARD);
 }
 
 int main() {
 
-	char test[INPUT_BUFFER] = "ls  -all $HOME   /home";
+	char test[INPUT_BUFFER] = "ls  -all $HOME   /home | echo \"Miikka\"";
 	char test2[INPUT_BUFFER] = "echo  $HOME";
 
 	int i = 0;
