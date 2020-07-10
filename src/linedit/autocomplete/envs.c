ff91680651250aa354d7d399c6e77e7f7531f87b
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2020-07-10 12:47:40 +0300

    refactored envs.c

--- src/linedit/autocomplete/envs.c
+++ src/linedit/autocomplete/envs.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 08:03:03 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/07/09 17:16:16 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/07/10 12:47:23 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 static int get_envname_last_index(char *str)
 {
-	char *cpy = str;
-	int i = 0;
+	char *cpy;
+	int i;
+
+	cpy = str;
+	i = 0;
 	while (cpy[i] && cpy[i] != '=')
 		i++;
 	return (i);
@@ -36,32 +39,6 @@ static int count_envs(char **envs)
 	return (i);
 }
 
-void load_envs(t_completer *ac, char **envs)
-{
-	char **cpy;
-	int i;
-
-	ac->envs_count = count_envs(envs);
-	if (!(ac->envs = malloc(sizeof(char *) * (ac->envs_count + 1))))
-		return ;
-	char **head = ac->envs;
-	i = 0;
-	while (i < ac->envs_count)
-	{
-		int last = get_envname_last_index(envs[i]);
-		// if (!last)
-		// 	last = 1;
-		ac->envs[i] = malloc(sizeof(char) * last + 1);
-		ft_strncpy(ac->envs[i], envs[i], last);
-		ac->envs[i][last] = '\0';
-		//ft_printf("\n%d\n", i);
-		i++;
-		// ft_printf("\nalive\n");
-	}
-	ac->envs[i] = NULL;
-	ac->envs = head;
-}
-
 static int	ft_env_exists(char *name, char *given, int len_given)
 {
 	if (ft_strncmp(name, given, len_given) == 0 \
@@ -70,15 +47,39 @@ static int	ft_env_exists(char *name, char *given, int len_given)
 	return (0);
 }
 
+void load_envs(t_completer *ac, char **envs)
+{
+	char **cpy;
+	char **head;
+	int i;
+	int last;
+
+	ac->envs_count = count_envs(envs);
+	if (!(ac->envs = malloc(sizeof(char *) * (ac->envs_count + 1))))
+		return ;
+	head = ac->envs;
+	i = 0;
+	while (i < ac->envs_count)
+	{
+		last = get_envname_last_index(envs[i]);
+		ac->envs[i] = malloc(sizeof(char) * last + 1);
+		ft_strncpy(ac->envs[i], envs[i], last);
+		ac->envs[i][last] = '\0';
+		i++;
+	}
+	ac->envs[i] = NULL;
+	ac->envs = head;
+}
+
 char		*ft_getenv(char *name, char **envs)
 {
 	int i;
 	int len;
 
-	i = 0;
 	len = ft_strlen(name);
 	if (!name || ft_strequ(name, "$"))
 		return (NULL);
+	i = 0;
 	while (envs[i])
 	{
 		if (ft_env_exists(envs[i], name, len))
