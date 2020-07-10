2c0a61ce089dad3ddd6369b97bbcd9c5bb5713ce
Miikka Tuominen <mtuomine@c2r5p13.hive.fi>
2020-07-10 15:03:50 +0300

    cleaned prompt.c

--- src/linedit/prompt.c
+++ src/linedit/prompt.c
@@ -6,17 +6,17 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 09:26:50 by mtuomine          #+#    #+#             */
-/*   Updated: 2020/07/09 22:46:37 by mtuomine         ###   ########.fr       */
+/*   Updated: 2020/07/10 15:02:41 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "linedit.h"
 
 /*
 ** Returns last item after '/
 */
 
-#include "linedit.h"
-
-static char *deepest_folder(char *path)
+static char	*deepest_folder(char *path)
 {
 	int delta;
 	int i;
@@ -36,26 +36,28 @@ static char *deepest_folder(char *path)
 		return (NULL);
 	if (path[0] && path[1])
 		delta--;
-	return (ft_strdup(path+(len-delta)));
+	return (ft_strdup(path + (len - delta)));
 }
 
-static char *git_branch(char *cwd)
+static char	*git_branch(char *cwd)
 {
-	char *line;
-	char *path = ft_pathjoin(cwd, ".git/HEAD");
-	int fd = open(path, O_RDONLY);
-	free(path);
+	char	*line;
+	char	*path;
+	char	*branch;
+	int		fd;
 
+	path = ft_pathjoin(cwd, ".git/HEAD");
+	fd = open(path, O_RDONLY);
+	free(path);
 	if (fd == -1)
 		return (NULL);
-
 	get_next_line(fd, &line);
-	char *branch = deepest_folder(line);
+	branch = deepest_folder(line);
 	free(line);
 	return (branch);
 }
 
-static int handle_readmore(t_line *line)
+static int	handle_readmore(t_line *line)
 {
 	if (!line->readmore)
 		return (0);
@@ -69,53 +71,7 @@ static int handle_readmore(t_line *line)
 	return (1);
 }
 
-// void		print_prompt(t_line *line)
-// {
-// 	// char	host[INPUT_BUFFER + 1];
-// 	// gethostname(host, INPUT_BUFFER);
-// 	char *prompt;
-// 	char *branch;
-// 	char *temp;
-// 	char *temp2;
-// 	char	cwd[INPUT_BUFFER + 1];
-
-// 	ft_bzero(line->prompt, INPUT_BUFFER);
-// 	if (handle_readmore(line))
-// 		return ;
-
-// 	getcwd(cwd, INPUT_BUFFER);
-
-// 	// ft_printf("\n%s\n", line->ac->paths[0]);
-// 	// ft_printf("\n%s\n", line->ac->paths[1]);
-// 	// ft_printf("\n%s\n", line->ac->paths[2]);
-
-// 	char *dir = deepest_folder(cwd);
-// 	if (!(branch = git_branch(cwd)))
-// 	{
-// 		prompt = ft_strjoin(dir, "$>");
-// 	}
-// 	else
-// 	{
-// 		temp = ft_strjoin(dir, " on git:");
-// 		temp2 = ft_strjoin(temp, branch);
-// 		prompt = ft_strjoin(temp2, "$>");
-// 		free(temp);
-// 		free(temp2);
-// 		free(branch);
-// 	}
-// 	free(dir);
-
-// 	ft_strcpy(line->prompt, prompt);
-// 	free(prompt);
-// 	line->prompt_len = ft_strlen(line->prompt);
-// 	ft_putstr(FT_GREEN);
-// 	if (!line->was_copy)
-// 		ft_putstr(line->prompt);
-// 	ft_putstr(FT_RESET);
-// 	line->was_copy = 0;
-// }
-
-void print_prompt(t_line *line)
+void		print_prompt(t_line *line)
 {
 	if (line->readmore)
 	{
@@ -129,11 +85,51 @@ void print_prompt(t_line *line)
 		line->was_copy = 0;
 		return ;
 	}
-
-	//gethostname(host, INPUT_BUFFER);
-	// ft_bzero(line->prompt, INPUT_BUFFER);
 	ft_putstr(FT_GREEN);
 	if (!line->was_copy)
 		ft_putstr((const char *)line->prompt);
 	ft_putstr(FT_RESET);
 }
+
+/*
+** void		print_prompt(t_line *line)
+** {
+** 	// char	host[INPUT_BUFFER + 1];
+** 	// gethostname(host, INPUT_BUFFER);
+** 	char *prompt;
+** 	char *branch;
+** 	char *temp;
+** 	char *temp2;
+** 	char	cwd[INPUT_BUFFER + 1];
+** 	ft_bzero(line->prompt, INPUT_BUFFER);
+** 	if (handle_readmore(line))
+** 		return ;
+** 	getcwd(cwd, INPUT_BUFFER);
+** 	// ft_printf("\n%s\n", line->ac->paths[0]);
+** 	// ft_printf("\n%s\n", line->ac->paths[1]);
+** 	// ft_printf("\n%s\n", line->ac->paths[2]);
+** 	char *dir = deepest_folder(cwd);
+** 	if (!(branch = git_branch(cwd)))
+** 	{
+** 		prompt = ft_strjoin(dir, "$>");
+** 	}
+** 	else
+** 	{
+** 		temp = ft_strjoin(dir, " on git:");
+** 		temp2 = ft_strjoin(temp, branch);
+** 		prompt = ft_strjoin(temp2, "$>");
+** 		free(temp);
+** 		free(temp2);
+** 		free(branch);
+** 	}
+** 	free(dir);
+** 	ft_strcpy(line->prompt, prompt);
+** 	free(prompt);
+** 	line->prompt_len = ft_strlen(line->prompt);
+** 	ft_putstr(FT_GREEN);
+** 	if (!line->was_copy)
+** 		ft_putstr(line->prompt);
+** 	ft_putstr(FT_RESET);
+** 	line->was_copy = 0;
+** }
+*/
